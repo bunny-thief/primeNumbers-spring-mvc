5ad358c96807ff3a4a70ef1d5d8761b53e69e777
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-01-23 22:23:37 +0100

    Initial commit

---
+++ src/main/java/dev/passingarguments/primenumbersmvc/PrimeNumbersMvcApplication.java
@@ -0,0 +1,13 @@
+package dev.passingarguments.primenumbersmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeNumbersMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeNumbersMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/passingarguments/primenumbersmvc/PrimeNumbersMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.passingarguments.primenumbersmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeNumbersMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
