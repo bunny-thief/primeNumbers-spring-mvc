9a236bb67743ac50388b599461820d15aeb83598
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-01-24 14:20:23 +0100

    Add functionality that displays all prime numbers given a range
    - Add the name attribute 'getRange' to the 'Primes in Range' button in 'main.html'
    - Add @PostMapping that checks if the name attribute equals 'getRange'
    - Rediret users to 'get-range.html'
    - Accept a start and end numbers
    - Determine prime numbers that in between start and end numbers
    - Add the resulting message as an attribute to the model
    - Display message in 'display-result.html'

--- src/main/java/dev/passingarguments/primenumbersmvc/controllers/MainController.java
+++ src/main/java/dev/passingarguments/primenumbersmvc/controllers/MainController.java
@@ -1,7 +1,6 @@
 package dev.passingarguments.primenumbersmvc.controllers;
 
-import dev.passingarguments.primenumbers.*;
-
+import dev.passingarguments.primenumbers.PrimeNumbers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -44,6 +43,36 @@ public class MainController {
         int nextPrime = PrimeNumbers.nextPrime(number);
         String message = String.format("%d is the next prime number after %d.", nextPrime, number);
 
+        model.addAttribute("message", message);
+
+        return "display-result";
+    }
+
+    @PostMapping(path = "/result", params = "getRange")
+    public String showGetRange() {
+        return "get-range";
+    }
+
+    @PostMapping(path = "/result", params = "primesInRange")
+    public String showResultPrimesInRange(@RequestParam("firstNumber") int firstNumber,
+                                          @RequestParam("secondNumber") int secondNumber,
+                                          Model model) {
+
+        String message = String.format("Prime numbers between %d and %d are:\n", firstNumber, secondNumber);
+        int[] primes = PrimeNumbers.primesInRange(firstNumber, secondNumber);
+
+        for (int i = 0; i < primes.length; i++) {
+            message += primes[i];
+
+            if (i < primes.length - 1) {
+                message += ", ";
+            } else {
+                message += ".";
+                break;
+            }
+        }
+
+        System.out.println(message);
         model.addAttribute("message", message);
 
         return "display-result";
