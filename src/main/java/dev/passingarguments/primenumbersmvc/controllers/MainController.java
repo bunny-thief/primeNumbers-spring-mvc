e5d0f3b0e4174ee9e8daf2263ee8798a0217a8ae
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-01-24 09:31:13 +0100

    Add main.html

--- src/main/java/dev/passingarguments/primenumbersmvc/controllers/MainController.java
+++ src/main/java/dev/passingarguments/primenumbersmvc/controllers/MainController.java
@@ -2,14 +2,19 @@ package dev.passingarguments.primenumbersmvc.controllers;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String showHome() {
         return "home";
+    }
+
+    @GetMapping("/main")
+    public String showMain() {
+        return "main";
     }
 
 }
